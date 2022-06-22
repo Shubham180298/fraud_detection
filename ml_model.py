a47d8e1e01467c17b6845f96713722ea63232312
Shubham180298 <shubhamgupta0218@gmail.com>
2022-06-22 18:10:35 +0530

    3 models added

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print(" model file of machine learning")
+
+
+def model1():
+    print("model 1 created")
+
+def model2():
+    print("model 2 created")
+
+def model3():
+    print("model 3 created")
